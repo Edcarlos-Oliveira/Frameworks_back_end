0a52617ba3f5933dc1da3d2c7f11f418e68b6346
Edcarlos Oliveira <112654995+Edcarlos-Oliveira@users.noreply.github.com>
2024-06-26 20:38:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Frameworks_back_end
+ Tabela de frameworks mais utilizados.
---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Alerta
+function alerta() {
+    window.alert('Espero que a documentação esteja atualizada,\nsenão clique em Saber Mais!');
+}
